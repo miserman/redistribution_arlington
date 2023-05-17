2f64afd0e6fa9fef11de79202618a2ce3a91136d
Micah <micah.iserman@gmail.com>
2023-05-17 09:35:05 -0400

    rebuilds with package updates

--- build.R
+++ build.R
@@ -1,12 +1,16 @@
 library(community)
 
-file.copy("../estimate_comparison/results_tract.csv", "docs/data/results.csv", TRUE)
+file.copy(
+  "../estimate_comparison/results_tract.csv",
+  "../redistribution_arlington/docs/data/results.csv",
+  TRUE
+)
 data_add(
   "results.csv",
   list(ids = list(variable = "geoid")),
-  dir = "../redistribution_arlington/docs/data/", refresh = TRUE
+  dir = "../redistribution_arlington/docs/data/"
 )
 
-site_build('../redistribution_arlington', serve = TRUE, options = list(
+site_build("../redistribution_arlington", serve = TRUE, options = list(
   theme_dark = TRUE, color_scale_center = "median", palette = "purple"
 ))
